f0aaa360036981c4091ec5c39777d378f4ea956e
Wolf <wolf@Tatooine.local>
2021-05-09 16:34:33 +1000

    error handling middleware complete and tested with Postman

---
+++ auth/src/errors/custom-error.ts
@@ -0,0 +1,15 @@
+export abstract class CustomError extends Error {
+    abstract statusCode: number;
+
+    constructor(message: string) {
+        super(message);
+
+        // only because we're extending a built in class
+        Object.setPrototypeOf(this, CustomError.prototype);
+    };
+
+    abstract serialiseErrors(): {
+            message: string;
+            field?: string;
+        }[]
+};
