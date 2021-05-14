d416888b024ae14e793e82143ebe29a22ba40fb6
Wolf <wolf@Tatooine.local>
2021-05-14 14:52:36 +1000

    auth routes, middlewares and error handling complete

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,62 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+
+import { validateEmailPwdRequest } from "../middlewares/validate-email-pwd-request";
+
+import { BadRequestError } from "../errors/bad-request-error";
+
+import { User } from "../models/user";
+
+import { Password } from "../services/password";
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Invalid email or password"),
+    body("password")
+      .trim() //remove leading and trailing spaces
+      .notEmpty()
+      .withMessage("Invalid email or password"),
+  ],
+  validateEmailPwdRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
-export { router as signinRouter};
+    const existingUser = await User.findOne({ email });
+
+    // user does not exist
+    if (!existingUser) {
+      throw new BadRequestError("Invalid email or password");
+    }
+
+    // compare supplied and stored passwords
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError("Invalid email or password");
+    }
+
+    // generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it on the session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
+
+export { router as signinRouter };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,55 +1,53 @@
-import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateEmailPwdRequest } from "../middlewares/validate-email-pwd-request";
 
-import { User } from '../models/user';
+import { BadRequestError } from "../errors/bad-request-error";
+
+import { User } from "../models/user";
 
 const router = express.Router();
 
-router.post('/api/users/signup',
-[
-    body('email')
-        .isEmail()
-        .withMessage('Please provide a valid email'),
-    body('password')
-        .trim()  //remove leading and trailing spaces
-        .isLength({ min: 4, max: 20})
-        .withMessage('Your password must have 4-20 characters')
-], 
-async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    };
-
-    const {email, password} = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Please provide a valid email"),
+    body("password")
+      .trim() //remove leading and trailing spaces
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Your password must have 4-20 characters"),
+  ],
+  validateEmailPwdRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
 
     const existingUser = await User.exists({ email });
-    
+
     if (existingUser) {
-        throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
-    const user = User.build({email, password});
+    const user = User.build({ email, password });
     await user.save();
 
     // generate JWT
-    const userJwt = jwt.sign({
+    const userJwt = jwt.sign(
+      {
         id: user.id,
-        email: user.email
-    }, process.env.JWT_KEY!
+        email: user.email,
+      },
+      process.env.JWT_KEY!
     );
 
     // store it on the session object
     req.session = {
-        jwt: userJwt
+      jwt: userJwt,
     };
 
     res.status(201).send(user);
-});
+  }
+);
 
-export { router as signupRouter};
+export { router as signupRouter };
